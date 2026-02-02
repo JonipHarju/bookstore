bb6b5ce83915da6807878c06e8083cca701297b8
jonipHarju <jonipharju@gmail.com>
2026-02-02 17:53:04 +0200

    feat: create BookController that handles get requests to /index that renders bookstore view

--- src/main/java/fi/joniharju/bookstore/controller/BookController.java
+++ src/main/java/fi/joniharju/bookstore/controller/BookController.java
@@ -1,5 +1,16 @@
 package fi.joniharju.bookstore.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BookController {
+
+    @GetMapping("/index")
+    public String indexPage(Model model) {
+
+        return "bookstore";
+    }
 
 }
