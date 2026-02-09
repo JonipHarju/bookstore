ec10f1a0f48c880dfb91e0b6b403b9d1a9383159
jonipHarju <jonipharju@gmail.com>
2026-02-09 13:37:45 +0200

    feat: add functionality to create and save new books

--- src/main/java/fi/joniharju/bookstore/controller/BookController.java
+++ src/main/java/fi/joniharju/bookstore/controller/BookController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import fi.joniharju.bookstore.model.Book;
 import fi.joniharju.bookstore.model.BookRepository;
 
 @Controller
@@ -29,6 +31,18 @@ public class BookController {
     @GetMapping("/delete/{id}")
     public String deleteBook(@PathVariable("id") Long id) {
         repository.deleteById(id);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addBook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(Book book) {
+        repository.save(book);
         return "redirect:/booklist";
     }
 
