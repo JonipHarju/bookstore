c758ef2c4d4c373b44bf6c89a092c5ad5dfd5eb3
jonipHarju <jonipharju@gmail.com>
2026-02-09 12:21:32 +0200

    feat: add /booklist route that lists all the books

--- src/main/java/fi/joniharju/bookstore/controller/BookController.java
+++ src/main/java/fi/joniharju/bookstore/controller/BookController.java
@@ -4,13 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import fi.joniharju.bookstore.model.BookRepository;
+
 @Controller
 public class BookController {
+    private BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/index")
     public String indexPage(Model model) {
-
         return "bookstore";
+    }
+
+    @GetMapping("/booklist")
+    public String getBookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 
 }
